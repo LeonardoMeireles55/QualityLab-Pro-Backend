ab3387418866c251e14f330536280c515846a7e2
leonardo <leomeireles55@hotmail.com>
2023-12-12 15:18:16 -0300

    Cors fixes

--- src/main/java/leonardo/labutilities/qualitylabpro/infra/security/SecurityConfiguration.java
+++ src/main/java/leonardo/labutilities/qualitylabpro/infra/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,6 +30,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers(HttpMethod.POST, "/user/signIn").permitAll();
@@ -37,7 +39,7 @@ public class SecurityConfiguration {
                     req.requestMatchers(HttpMethod.PATCH, "/user/update/password").permitAll();
                     req.requestMatchers
                             ("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**","/swagger-ui").permitAll();
-                    req.anyRequest().permitAll();
+//                    req.anyRequest().permitAll();
 
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
