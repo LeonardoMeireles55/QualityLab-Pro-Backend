e51ec1f1fe92a6da8addecd2485339e59d3ec9c1
leonardo <leomeireles55@hotmail.com>
2023-12-25 14:31:06 -0300

    Cors tweaks

--- src/main/java/leonardo/labutilities/qualitylabpro/infra/security/CorsConfig.java
+++ src/main/java/leonardo/labutilities/qualitylabpro/infra/security/CorsConfig.java
@@ -10,8 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5500")
-                .allowedOrigins("**")
-                .allowedOrigins("null")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
