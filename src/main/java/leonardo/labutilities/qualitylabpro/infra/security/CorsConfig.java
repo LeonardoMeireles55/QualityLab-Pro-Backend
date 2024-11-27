6077a325e4e1d6c960ea9c6ec93478f3b8bd27dc
leonardomeireles55 <leomeireles55@hotmail.com>
2024-11-27 16:07:06 -0300

    refactor: optimized code for performance and apply best practices.

--- src/main/java/leonardo/labutilities/qualitylabpro/infra/security/CorsConfig.java
+++ src/main/java/leonardo/labutilities/qualitylabpro/infra/security/CorsConfig.java
@@ -1,4 +1,5 @@
 package leonardo.labutilities.qualitylabpro.infra.security;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
