1aafb24e757770c09721b8653e4ecf1980290e6b
leonardomeireles55 <leomeireles55@hotmail.com>
2024-12-19 14:37:26 -0300

    refactor: tweaked token exp time.

--- src/main/java/leonardo/labutilities/qualitylabpro/service/users/TokenService.java
+++ src/main/java/leonardo/labutilities/qualitylabpro/service/users/TokenService.java
@@ -42,6 +42,6 @@ public class TokenService {
     }
 
     private Instant dateExp() {
-        return LocalDateTime.now().plusHours(72).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusHours(12).toInstant(ZoneOffset.of("-03:00"));
     }
 }
