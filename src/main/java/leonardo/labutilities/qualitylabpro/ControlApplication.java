f6c675d74cd02de2738b47ece6b3b13bd635b39d
Leonardo Meireles <leomeireles55@hotmail.com>
2025-01-07 19:16:45 -0300

    refactor: update .gitignore and application properties for testing environment

--- src/main/java/leonardo/labutilities/qualitylabpro/ControlApplication.java
+++ src/main/java/leonardo/labutilities/qualitylabpro/ControlApplication.java
@@ -2,13 +2,12 @@ package leonardo.labutilities.qualitylabpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-//@EnableCaching
+// @EnableCaching
 @SpringBootApplication()
 public class ControlApplication {
 
-    public static void main(final String[] args) {
-        SpringApplication.run(ControlApplication.class, args);
-    }
+	public static void main(final String[] args) {
+		SpringApplication.run(ControlApplication.class, args);
+	}
 }
