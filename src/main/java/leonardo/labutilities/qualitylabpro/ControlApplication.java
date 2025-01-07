8019278590ecfefcdc8d8bbf5f8e72f7f22a08a2
Leonardo Meireles <leomeireles55@hotmail.com>
2025-01-07 19:45:44 -0300

    refactor: update database configuration to use environment variables and enable caching

--- src/main/java/leonardo/labutilities/qualitylabpro/ControlApplication.java
+++ src/main/java/leonardo/labutilities/qualitylabpro/ControlApplication.java
@@ -2,8 +2,9 @@ package leonardo.labutilities.qualitylabpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
-// @EnableCaching
+@EnableCaching
 @SpringBootApplication()
 public class ControlApplication {
 
